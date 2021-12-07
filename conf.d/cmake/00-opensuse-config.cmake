57b8234b43ad4abdeb2e5dd06a97901d4760ff2a
Ronan Le Martret <ronan.lemartret@iot.bzh>
2021-12-07 15:12:59 +0100

    Remove useless rpath
    rpath broke the build on openSUSE
    
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- conf.d/cmake/00-opensuse-config.cmake
+++ conf.d/cmake/00-opensuse-config.cmake
@@ -2,7 +2,3 @@ message(STATUS "Custom options: 00-opensuse-config.cmake --")
 
 # Libshell is not part of standard Linux Distro (https://libshell.org/)
 set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:/usr/local/lib64/pkgconfig")
-
-# Remember sharelib path to simplify test & debug
-set(BINDINGS_LINK_FLAG "-Xlinker -rpath=/usr/local/lib64")
-
